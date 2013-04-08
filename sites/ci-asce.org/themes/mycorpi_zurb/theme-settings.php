e5fae24262ba97bff3b91befb2fae871d0e12f2b
Marvin Oey (live) <moey@asce.org>
2013-04-08 17:50:48 -0500

    Major updates to modules

--- sites/ci-asce.org/themes/mycorpi_zurb/theme-settings.php
+++ sites/ci-asce.org/themes/mycorpi_zurb/theme-settings.php
@@ -7,5 +7,5 @@
  * @param $form_state
  *   The form state.
  */
-function zurb_foundation_theme_form_system_theme_settings_alter(&$form, &$form_state) {
+function mycopri_zurb_theme_form_system_theme_settings_alter(&$form, &$form_state) {
 }
